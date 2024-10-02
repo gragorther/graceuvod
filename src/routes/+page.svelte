44e0b5dd1cd4dd09b702ff72ff5d3545b5de25f0
Gregor <gregorpodjed@yahoo.com>
2024-10-02 19:32:19 +0200

    neki sem dodal >:3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,6 @@
 	<h1 class="bg-yellow-200 font-black text-2xl p-5 border-red-300 border-2 rounded-md text-center">
 		Hello World, Hello Grace! hello gregor
 	</h1>
+	<p class="text-violet-500 text-xl">besedilo</p>
 	<img class="rounded-md w-96" src="art.webp" />
 </div>
